9d5eb558b529f4bd329856226f4e8c128a01b748
fredrikjsjolin <124003600+fredrikjsjolin@users.noreply.github.com>
2023-03-22 20:47:25 +0100

    Update Stensaxpåse.py

--- Stensaxpåse.py
+++ Stensaxpåse.py
@@ -1,24 +1,18 @@
 import random
 
 list = ["sten", "sax", "påse"]
-
 random_word = random.choice(list)
-   
 my_choice = input("Sten, sax eller påse: ")
-
 print("Datorns val:", random_word)
 
 while my_choice == random_word:
     print("Oavgjort, försök igen")
-
     random_word = random.choice(list)
-   
     my_choice = input("Sten, sax eller påse: ")
-
     print("Datorns val:", random_word)
 
 if random_word == "sten" and my_choice == "påse" or random_word == "sax" and my_choice == "sten" or random_word == "påse" and my_choice == "sax":
-        print("Du vann!")
+    print("Du vann!")
 
 else:
-        print("Du förlorade..")
+    print("Du förlorade..")
